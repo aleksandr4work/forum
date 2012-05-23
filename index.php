a4aab3be4412b03258dfc62a337fc2928603fd15
Firstname Lastname <aleksandr4work>
2012-05-24 00:42:37 +0700

    2222

--- index.php
+++ index.php
@@ -360,4 +360,45 @@ function smf_main()
    global $linkfeed;
 
    echo $linkfeed->return_links(2);
+   
+   
+
+// THE FOLLOWING BLOCK IS USED TO RETRIEVE AND DISPLAY LINK INFORMATION.
+// PLACE THIS ENTIRE BLOCK IN THE AREA YOU WANT THE DATA TO BE DISPLAYED.
+
+// MODIFY THE VARIABLES BELOW:
+// The following variable defines whether links are opened in a new window
+// (1 = Yes, 0 = No)
+$OpenInNewWindow = "1";
+
+// # DO NOT MODIFY ANYTHING ELSE BELOW THIS LINE!
+// ----------------------------------------------
+$BLKey = "A66U-QK08-GEWD";
+
+$QueryString  = "LinkUrl=".urlencode((($_SERVER['HTTPS']=='on')?'https://':'http://').$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+$QueryString .= "&Key=" .urlencode($BLKey);
+$QueryString .= "&OpenInNewWindow=" .urlencode($OpenInNewWindow);
+
+
+if(intval(get_cfg_var('allow_url_fopen')) && function_exists('readfile')) {
+    @readfile("http://brokerage.linkadage.com/engine.php?".$QueryString); 
+}
+elseif(intval(get_cfg_var('allow_url_fopen')) && function_exists('file')) {
+    if($content = @file("http://brokerage.linkadage.com/engine.php?".$QueryString)) 
+        print @join('', $content);
+}
+elseif(function_exists('curl_init')) {
+    $ch = curl_init ("http://brokerage.linkadage.com/engine.php?".$QueryString);
+    curl_setopt ($ch, CURLOPT_HEADER, 0);
+    curl_exec ($ch);
+
+    if(curl_error($ch))
+        print "Error processing request";
+
+    curl_close ($ch);
+}
+else {
+    print "It appears that your web host has disabled all functions for handling remote pages and as a result the BackLinks software will not function on your web page. Please contact your web host for more information.";
+}
+
 ?>
